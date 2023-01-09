099fa12985793dd086250feac1ed85f0288c333d
158 <15848706256@163.com>
2023-01-10 00:16:03 +0800

    试岗前端

---
+++ src/api/userInfo.js
@@ -0,0 +1,60 @@
+import request from '@/utils/request'
+
+const api_name = "userInfo"
+
+export default {
+  //更改用户状态
+  changeStates(id,status){
+    return request({
+      //接口路径
+      url: `${api_name}`+"/updateStatus/"+`${id}`+"/"+`${status}`,
+      method: 'get',
+    })
+  },
+  //列表
+  getPageList(page,limit,queryObj){
+    return request({
+      //接口路径
+      url: `${api_name}`+"/"+`${page}`+"/"+`${limit}`,
+      method: 'get',
+      params:queryObj
+    })
+
+  },
+  save(queryObj){
+    return request({
+      //接口路径
+      url: `${api_name}`+"/save",
+      method: 'post',
+      data:queryObj
+    })
+  },
+
+  //修改-根据id查询
+  getById(id){
+    return request({
+      //接口路径
+      url: `${api_name}`+"/getUser/"+`${id}`,
+      method: 'get',
+    })
+  },
+
+  //修改
+  update(queryObj){
+    return request({
+      //接口路径
+      url: `${api_name}`+"/update",
+      method: 'put',
+      data:queryObj
+    })
+  },
+  //删除
+  remove(id){
+    return request({
+      //接口路径
+      url: `${api_name}`+"/remove/"+`${id}`,
+      method: 'delete',
+    })
+  }
+
+}
---
+++ src/views/testWork/list.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+  <h1>7777</h1>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
